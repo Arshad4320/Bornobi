a6a51a5ac60b965e0900b249547dd6314413393a
Arshad Ullah <mdarshadullah835@gmail.com>
2024-02-04 20:04:34 +0600

    some update

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,37 +1,33 @@
 import React from "react";
 import Headding from "../../components/Heading/Heading";
 
-import img from "../../assets/img/property-1.jpg";
-import img2 from "../../assets/img/electrician.avif";
-import img3 from "../../assets/img/Handy.jpg";
-import img4 from "../../assets/img/management.jpg";
 import Pragraph from "./../../components/Pragraph/Pragraph";
 
 const pragrapContent = [
   {
     id: 1,
     title:
-      "Bornobi Properties is an independent Development Agent, that specialises in Development, lettings and Management. Our local knowledge and connection make us experts within our industry.",
+      "Bornobi Properties is an independent Property services company that specialises in Renovation, Short lettings, Service Accommodation Management and Maintain with nationwide builders recutting to provide the best services in local area within our experience and expertise. ",
   },
   {
     id: 2,
     title:
-      "We have a vast knowledge and experience within the urban property industry plus the local locations that we deal with. We are happy to answer any questions that you might have, if you are thinking of development, renovation, letting, renting or management services.",
+      "We have a vast knowledge and experience within the urban property industry plus the local locations that we deal with. We are happy to answer any questions that you might have, if you are thinking of development, renovation, Short-letting, Maintain, Management, SA and OTA services. ",
   },
   {
     id: 3,
     title:
-      "Our teams are ready to carry out many viewings and valuations on a daily basis to support your demand within the office hour or 24 hours on online massages. These services will make sure you are never miss an opportunity to rent or let your property.",
+      "Our Rapid Teams are ready to carry out many viewings and valuations on a daily basis to support your demand within the service our 8 AM to 10 PM   hour and 24 Hours Call Back support. ",
   },
   {
     id: 4,
     title:
-      "We are totally digitalized for our marketing and advertised to reach and engaged the clients. We are on all the major following platforms and medias to show our property and services:",
+      "These services will make sure you are never missing an opportunity to get our Rapid Repair services to Manage & Maintain your property 24/7. ",
   },
   {
     id: 5,
     title:
-      "Rightmove, Zoopla, Prime Location, property live, Google, MSN, our website and social media.",
+      "We are totally digitalized for our marketing and advertised to reach and engaged the clients. We are on all the major following platforms and medias to show our services including- Google, social media, Prime Location, Yellow page and our website.",
   },
 ];
 
